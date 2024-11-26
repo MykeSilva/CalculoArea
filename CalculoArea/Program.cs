8d219ef2d8402053362e1dd48db1b3bfef90d4f4
Myke Silva <119154150+Spike2247@users.noreply.github.com>
2024-11-25 21:41:10 -0300

    first commit

---
+++ CalculoArea/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace CalculoArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, PrecoMetroQuadrado, area, preco;
+
+            Console.WriteLine("Digite a largura do terreno");
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o comprimento do terreno");
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o preço do M²");
+            PrecoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            area = largura * comprimento;
+            preco = area * PrecoMetroQuadrado;
+
+            Console.WriteLine("AREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("O valor do preço do seu terreno é R$ " + preco.ToString("F2", CultureInfo.InvariantCulture));
+            
+
+        }
+    }
+}
